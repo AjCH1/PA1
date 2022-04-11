cd6f8b107aed49fd185e491d1476f2efc3710764
AjCH1 <ajchen4@uci.edu>
2022-04-11 01:13:31 -0700

    Product Card

--- ItemCard.js
+++ ItemCard.js
@@ -1,1 +1,10 @@
-var listOfItems = ["a", "b", "c"];
+var listOfItems = 
+[
+    new Item("Air Jordans 5", "Nike", "Converse", "$220.00", "description", "21 reviews", ["images/Air Jordan 5.jpg"]),
+    new Item("b", "companyB", "Converse", "$20.00", "description", "21 reviews", ["images/Air Jordan 5.jpg"])
+];
+
+
+function createProductCard(){
+    //create a product card using Item data
+}
--- item.js
+++ item.js
@@ -1,9 +1,14 @@
 class Item{
 
-    constructor(name, company, price, description){
+    productImages = [];
+
+    constructor(name, company, category, price, description, review, productImages){
         this.name = name;
         this.company = company;
+        this.category = category;
         this.price = price;
         this.description = description;
+        this.review = review;
+        this.productImages = productImages;
     }
 }
